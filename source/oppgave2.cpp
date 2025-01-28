9f170effe4fcd2dfd6c1d1a442c14fbfa6c52874
Alan <alanhaugen@gmail.com>
2025-01-28 21:56:08 +0100

    Scenes readded. red and green added to oppgave2

--- source/oppgave2.cpp
+++ source/oppgave2.cpp
@@ -33,6 +33,7 @@ IDrawable *oppgave2::LoadFromFile(String filename)
     float x;
     float y;
     float z;
+    float red, green;
 
     while ((read = getline(&line, &len, dataFile)) != -1)
     {
@@ -51,7 +52,12 @@ IDrawable *oppgave2::LoadFromFile(String filename)
             z = atof(vertex.SubString(vertex.IndexOf(" ", vertex.IndexOf(" ") + 1),
                                       vertex.IndexOf(" ", vertex.IndexOf(" ", vertex.IndexOf(" ") + 1) + 1)).ToChar());
 
-            vertices.Add(IDrawable::Vertex(glm::vec3(x, y, z), glm::vec4(0.0f, 1.0f, 1.0f, 1.0f)));
+            red = atof(vertex.SubString(vertex.IndexOf(" ") + 1,
+                                        vertex.IndexOf(" ", vertex.IndexOf(" ") + 1)).ToChar());
+            green = atof(vertex.SubString(vertex.IndexOf(" ") + 1,
+                                          vertex.IndexOf(" ", vertex.IndexOf(" ") + 1)).ToChar());
+
+            vertices.Add(IDrawable::Vertex(glm::vec3(x, y, z), glm::vec4(red, green, 1.0f, 1.0f)));
 
             if (firstPoint == true)
             {
